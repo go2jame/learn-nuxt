0c5fbff48a3b8952f792d049e05aebcdde0ef5a6
Thananan Chaisiriphutthichok <70027865@oishigroup.com>
2024-03-14 16:37:17 +0700

    2nd commit

---
+++ pages/todos/[id].vue
@@ -0,0 +1,20 @@
+<script setup>
+  const route = useRoute();
+  const todoId = route.params.id;
+
+  const BASE_URL = "https://65f2c139105614e6549eafb0.mockapi.io";
+
+  const { pending, data: todo } = useFetch(`${BASE_URL}/todos/${todoId}`, {
+    lazy: true,
+  });
+  
+</script>
+
+<template>
+  <nuxt-link to="/">Back</nuxt-link>
+  <div v-if="pending">Loading</div>
+  <div v-else>
+    <div>Todo {{ todo.id }}</div>
+    <div>{{ todo.name }}</div>
+  </div>
+</template>
---
+++ pages/todos/index.vue
@@ -0,0 +1,77 @@
+<script setup>
+  const BASE_URL = 'https://65f2c139105614e6549eafb0.mockapi.io'
+
+  const statuses = ['Pending', 'Doing', 'Done']
+  const todoText = ref('')
+  const isLoading = ref(true)
+  const todos = ref([])
+
+  const loadTodo = async () => {
+    isLoading.value = true
+    const { data } = useFetch(`${BASE_URL}/todos`, {
+      lazy: true
+    })
+    console.log(data)
+    todos.value = data.value
+    isLoading.value = false
+  }
+
+  const addTodo = async () => {
+    try {
+      isLoading.value = true
+      const todoData = {
+        name: todoText.value,
+        status: 'Pending'
+      }
+      await useFetch(`${BASE_URL}/todos`, {
+        method: 'post',
+        body: todoData
+      })
+      await loadTodo()
+    } catch (error) {
+      console.log('error', error)
+    }
+    isLoading.value = false
+  }
+
+  const deleteTodo = async (id) => {
+    try {
+      isLoading.value = true
+      await useFetch(`${BASE_URL}/todos/${id}`, {
+        method: 'delete'
+      })
+      await loadTodo()
+    } catch (error) {
+      console.log('error', error)
+    }
+
+    isLoading.value = false
+  }
+
+  // load in state setup
+  await loadTodo()
+</script>
+
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <div>
+      <input type="text" v-model="todoText" />
+      <button @click="addTodo">Add</button>
+    </div>
+    <ul v-if="!isLoading">
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.name }} status :
+        <select v-model="todo.status">
+          <option v-for="status in statuses" :key="status" :value="status">{{ status }}</option>
+        </select>
+
+        <NuxtLink :to="`/todos/${todo.id}`">
+          <button>Edit</button>
+        </NuxtLink>
+        <button @click="deleteTodo(todo.id)">DELETE</button>
+      </li>
+    </ul>
+    <div v-else>Loading</div>
+  </div>
+</template>
