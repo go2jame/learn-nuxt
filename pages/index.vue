e0c4cc9c31992eeb3d0ef9a7a50e0ae9c7c88de3
Thananan Chaisiriphutthichok <70027865@oishigroup.com>
2024-03-22 17:33:49 +0700

    testbyjame

--- pages/index.vue
+++ pages/index.vue
@@ -9,5 +9,6 @@ const route = useRoute()
     <h1>Nuxt Routing set up successfully!</h1>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    <h2>edit by James</h2>
   </div>
 </template>
